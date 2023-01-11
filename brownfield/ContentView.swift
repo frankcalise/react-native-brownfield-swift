3e1999d953b00b801e39e2a9713c1ff38eace937
Frank Calise <fcalise@gmail.com>
2023-01-11 12:31:35 -0500

    Initial Commit

---
+++ brownfield/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  brownfield
+//
+//  Created by Frank Calise on 1/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ brownfield/brownfieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  brownfieldApp.swift
+//  brownfield
+//
+//  Created by Frank Calise on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct brownfieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
